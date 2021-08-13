b4868bf8c870cbb8f54ca70dd6e703c3d68fa4ce
abdulloev011 <donyora53@gmail.com>
2021-08-13 10:04:20 +0500

    v1.0.4

--- dict/resources/views/main/index.blade.php
+++ dict/resources/views/main/index.blade.php
@@ -4,8 +4,9 @@
 <head>
   <meta charset="UTF-8">
   <title>Dictionary</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-  
+  <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  -->
+  <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
 </head>
 
 <body>
